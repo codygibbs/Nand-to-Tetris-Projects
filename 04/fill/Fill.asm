dcc11c7bc6ae1f1d0603ff6783613ae7b4aa6b09
Cody <codymgibbs@protonmail.com>
2024-08-27 23:26:28 -0500

    ch4 wip

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,68 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+	@KBD
+	D=M
+
+	@ON
+	D;JGT
+
+	@OFF
+	D;JEQ
+
+	// In case of timing issues
+	@LOOP
+	0;JMP
+
+(ON)
+	// Fill screen
+	@0
+	D=A-1
+
+	@R1
+	M=D
+
+	@FILL
+	0;JMP
+
+(OFF)
+	// Empty screen
+	@0
+	D=A
+
+	@R1
+	M=D
+
+	@FILL
+	0;JMP
+
+(FILL)
+	// R1 = fill value, R2 = current screen index (it counts backwards)
+	@16384
+	D=A
+	@SCREEN
+	D=D+A
+	@R2
+	M=D
+
+	(FILL_LOOP)
+		// Decrement index (R2)
+		@R2
+		MD=M-1
+
+		// Draw pixel
+		@R1
+		D=M
+		@R2
+		M=D
+		
+		@SCREEN
+		D=A
+		@R2
+		D=M-D
+		@FILL_LOOP
+		D;JEQ
+
+	@LOOP
+	0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -8,5 +8,40 @@
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+@R2
+M=0
 
-// Put your code here.
+@R0
+D=M
+@END
+D;JEQ
+
+@R1
+D=M
+@END
+D;JEQ
+
+(CONT)
+	@R2
+	D=M
+
+	// Add M (R0) to D, set D
+	@R0
+	D=D+M
+
+	@R2
+	M=D
+
+	// Decrement R1
+	@R1
+	M=M-1
+	D=M
+
+	// If R1 <= 0, jump to add operation
+	@CONT
+	D;JGT
+
+(END)
+	@END
+	0;JMP
+
